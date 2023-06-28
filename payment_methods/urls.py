d5e21c2540510a19a768720589feee20f1b837cb
Rayhun <rayhunkhan27@gmail.com>
2023-06-28 23:55:11 +0600

    update payment method urls

--- payment_methods/urls.py
+++ payment_methods/urls.py
@@ -4,15 +4,11 @@ from . import views
 
 urlpatterns = [
 
-
     path('payment_success', views.stripe_payment_success,name='thanks_page'),
     path('payment_cancel', views.payment_cancel,name='sorry_page'),
-
     path('pricing',views.pricing,name='pricing'),
     path('checkout/<int:plan_id>',views.checkout,name='checkout'),
     path('checkout_session/<int:plan_id>',views.checkout_session,name='checkout_session'),
      path('cancel_subscription',views.cancel_subscription,name='cancel_subscription'),
-
-
 
 ]
