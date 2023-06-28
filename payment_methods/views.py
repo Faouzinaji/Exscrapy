08df8ce33043ba855f8df53b29fc889dff7833e5
Rayhun <rayhunkhan27@gmail.com>
2023-06-28 23:54:50 +0600

    update payment method views

--- payment_methods/views.py
+++ payment_methods/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, redirect
 from authentication.models import Profile
 from .models import *
 import stripe
-from django.conf import settings
+from googleSearchScraper import settings
 from dateutil.relativedelta import relativedelta
 from Home.models import Wallet
 import datetime
@@ -44,8 +44,6 @@ def checkout(request, plan_id):
     user_wallet = Wallet.objects.get(user_id=users)
 
     if not Subscriber.objects.filter(user=request.user, status='Active').exists():
-
-
         return render(request, 'checkout.html', {'plan': planDetail,'user_wallet':user_wallet})
     else:
         error_message_text='You already have subscription of this ' + planDetail.title + ' plan.'
@@ -84,60 +82,57 @@ def checkout_session(request, plan_id):
 
 
 def stripe_payment_success(request):
+    # try:
+    session = stripe.checkout.Session.retrieve(request.GET['session_id'])
+    plan_id = session.client_reference_id
+    stripe_subscription_id = session.subscription
+    stripe_customer_id = session.customer
+    plan = Price_plan.objects.get(pk=plan_id)
+    user_profile = Profile.objects.get(owner=request.user)
+    user_wallet = Wallet.objects.get(user_id=user_profile)
+    user = request.user
     try:
-        print('Line no 51 done')
-        session = stripe.checkout.Session.retrieve(request.GET['session_id'])
-        plan_id = session.client_reference_id
-        stripe_subscription_id = session.subscription
-        stripe_customer_id = session.customer
-        print('customer id :', stripe_customer_id)
-        print('stripe subscription id:', stripe_subscription_id)
-        plan = Price_plan.objects.get(pk=plan_id)
-        user_profile = Profile.objects.get(owner=request.user)
-        user_wallet=Wallet.objects.get(user_id=user_profile)
-        user=request.user
-        try:
-            subsciber_details = Subscriber.objects.get(user=user, status='Active')
-            subsciber_details.status = 'Expire'
-            subsciber_details.save()
-            print('Line no 59 done')
-        except Exception as e:
-            pass
-
-        subsciption_from = datetime.date.today(),
-        subsciption_to = datetime.date.today() + relativedelta(months=1)
-
-
-
-        Subscriber.objects.create(
-            plan=plan,
-            user=user,
-            price=plan.price,
-            status='Active',
-            payment_method='Stripe',
-            subsciption_from=datetime.date.today(),
-            stripeCustomerId=stripe_customer_id,
-            stripeSubscriptionId=stripe_subscription_id,
-            subsciption_to=datetime.date.today() + relativedelta(months=1)
-
-        )
-
-        print('line no 76')
-        #updating the user wallet of no of lines
-        user_wallet.available_requests_balance+=plan.no_of_lines
-        user_wallet.save()
-        #creating a transaction
-        transaction_history.objects.create(user_id=user,plan=plan).save()
-        user_email = user.email
-        plan_title = plan.title
-        #send_payment_success_email(user_email, plan_title, subsciption_from, subsciption_to)
-
-        return render(request, 'payment_success.html',{'subsciption_from':subsciption_from,'subsciption_to':subsciption_to,'plan_title':plan_title,'user_wallet':user_wallet})
+        subsciber_details = Subscriber.objects.get(user=user, status='Active')
+        subsciber_details.status = 'Expire'
+        subsciber_details.save()
+        print('Line no 59 done')
     except Exception as e:
-        print('exception at line 83 is:', e)
-        users = Profile.objects.get(owner__email=request.user.email)
-        user_wallet = Wallet.objects.get(user_id=users)
-        return render(request, 'payment_cancel.html',{'user_wallet':user_wallet})
+        pass
+
+    subsciption_from = datetime.date.today(),
+    subsciption_to = datetime.date.today() + relativedelta(months=1)
+
+
+
+    Subscriber.objects.create(
+        plan=plan,
+        user=user,
+        price=plan.price,
+        status='Active',
+        payment_method='Stripe',
+        subsciption_from=datetime.date.today(),
+        stripeCustomerId=stripe_customer_id,
+        stripeSubscriptionId=stripe_subscription_id,
+        subsciption_to=datetime.date.today() + relativedelta(months=1)
+
+    )
+
+    print('line no 76')
+    #updating the user wallet of no of lines
+    user_wallet.available_requests_balance+=plan.no_of_lines
+    user_wallet.save()
+    #creating a transaction
+    transaction_history.objects.create(user_id=user,plan=plan).save()
+    user_email = user.email
+    plan_title = plan.title
+    #send_payment_success_email(user_email, plan_title, subsciption_from, subsciption_to)
+
+    return render(request, 'payment_success.html',{'subsciption_from':subsciption_from,'subsciption_to':subsciption_to,'plan_title':plan_title,'user_wallet':user_wallet})
+    # except Exception as e:
+    #     print('exception at line 83 is:', e)
+    #     users = Profile.objects.get(owner__email=request.user.email)
+    #     user_wallet = Wallet.objects.get(user_id=users)
+    #     return render(request, 'payment_cancel.html',{'user_wallet':user_wallet})
 
 
 def payment_cancel(request):
