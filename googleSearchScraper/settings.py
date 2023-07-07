0f6fdad2aed727354fe88d3f0d914d08b1fd1d27
Rayhun <rayhunkhan27@gmail.com>
2023-07-07 17:22:45 +0600

    update settings

--- googleSearchScraper/settings.py
+++ googleSearchScraper/settings.py
@@ -179,7 +179,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'media')
 
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': _CLOUD_NAME,
