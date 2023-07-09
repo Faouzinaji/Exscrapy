33f6ccec753ed9264bc4e9619224052f66e9c5d4
Rayhun <rayhunkhan27@gmail.com>
2023-07-10 01:51:06 +0600

    update admin

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,5 +1,26 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Profile
+from .models import Profile, Section, Price, Features, Landing
+
 admin.site.register(Profile)
+admin.site.register(Section)
+
+
+class FeaturesAdmins(admin.StackedInline):
+    model = Features
+    extra: int = 1
+    fields = ('title', )
+
+@admin.register(Price)
+class PriceAdmin(admin.ModelAdmin):
+    list_display = ('title', 'sub_title')
+    inlines = [FeaturesAdmins]
+
+
+@admin.register(Features)
+class FeaturesAdmin(admin.ModelAdmin):
+    list_display = ('title', 'to')
+
+
+admin.site.register(Landing)
