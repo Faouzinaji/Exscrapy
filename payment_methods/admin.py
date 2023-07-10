e98721d7edd718fa5841a45d690e1219e98dedf4
Rayhun <rayhunkhan27@gmail.com>
2023-07-11 02:41:18 +0600

    update payment methods

--- payment_methods/admin.py
+++ payment_methods/admin.py
@@ -2,16 +2,11 @@ from django.contrib import admin
 from .models import *
 
 
-
 class Price_planAdmin(admin.ModelAdmin):
 
     list_display = ('id','title','price','no_of_lines','description')
 
 admin.site.register(Price_plan,Price_planAdmin)
-
-
-
-
 
 
 class SubscriberAdmin(admin.ModelAdmin):
